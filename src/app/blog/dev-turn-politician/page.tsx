ee5247b5e8bf24e61c2459c05ca20e983f6a792b
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2024-05-04 02:08:18 +0530

    chore: added election blog

---
+++ src/app/blog/dev-turn-politician/page.tsx
@@ -0,0 +1,295 @@
+import { TracingBeam } from "@/components/ui/beam";
+import React from "react";
+
+{
+  /* <h2 className="text-xl font-semibold mb-4">
+What will I write about?
+</h2> */
+}
+// Heading 1
+
+{
+  /* <a
+href="https://www.paulgraham.com/articles.html"
+target="_blank"
+className="underline"
+>
+Paul Graham
+</a> */
+}
+// link
+
+const BlogPage = () => {
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-4">
+        [DRAFT 1] Geek guide to Politics
+      </h1>
+      {/* Blog Title ^^^^*/}
+      {/* <p className="text-lg mb-4">Welcome to my blog where I write about various topics.</p> */}
+      <p className="text-gray-600 mb-4">Date: May 3, 2024</p>
+      {/* Blog Date ^^^ */}
+
+      <div className="bg-black rounded-lg shadow-md ">
+        {/* <h2 className="text-xl font-semibold mb-4">Motivation</h2> */}
+        <p className="text-lg mb-4">
+        I will tell you how a geek can excel an election. I will try to
+          explain it through my personal experience. I know you will find it
+          worth it.
+        </p>
+        <h2 className="text-xl font-semibold mb-4">The Problem</h2>
+        <p className="text-lg mb-4">
+          The problems are one thing which motivates human to take action. My
+          set of problems were divided into 2 category:
+          <br />
+          <br />
+          1. Non-Democratic system <br />
+          2. My own communication issues
+          <br />
+          <br />
+          1) Let&apos;s talk about non-democratic system first. By system, I don&apos;t
+          mean the election process was rigged, but the way people take part in
+          it. The people don&apos;t seem to care about who was on council, and
+          whenever any stupid decision was taken by the administration, student
+          wrote angry words in their Instagram stories (which make no sense as
+          director doesn&apos;t follow you). The student representatives are
+          responsible to put student POV in front of the administration, but
+          they were not doing their job properly.
+          <br />
+          <br />
+          Below stated are steps to become a Student Representative:
+          <h1 className="ml-4">
+            <br />
+            1. In your 1st year, join a club with huge population that
+            does&apos;t do real work. (For IITR it is UBA, CDC, Students club
+            council, NSS, IRC just to name a few, and yes I just read my
+            opponent manifesto)
+            <br />
+            2. Then you pick a senior, who you think is the most popular in the
+            club, and hold most/highest Position of responsibilities (POR)
+            <br />
+            3. Then you talk, and talk to him and his friend as much as
+            possible. You do his chores, and you accompany him etc etc.
+            <br />
+            4. Then you start partying with him, you drink with him, you smoke
+            with him, you do everything with him.
+            <br />
+            5. It is a kind of trade, you do his work, and he will make you his
+            successor.
+          </h1>
+          <br />
+          But the problem with this system is that Hostel and college is never
+          the priority for the one in-charge and same for the disciple. And if
+          no-one question there dynastic rule, then the system will never
+          change, and condition will not improve. <br />
+          Until they feel that their position is challenged, they will not work
+          for the betterment of the hostel.
+          <br />
+          <br />
+          2) This may be quite personal, but I struggled with communication as a
+          child, rarely engaging with others. However, I&apos;ve made
+          significant progress since then and recognize there&apos;s still much
+          room for growth. For one, I am writing this very personal blog,
+          inviting everyone to inner-working of my mind and opening a room for
+          Criticism from complete strangers.
+          <br />
+          <br />
+          <h2 className="text-xl font-semibold mb-4">The obstacle</h2>
+          Aarsh Choudhary(Yes, we have same surname), he is the arch nemesis of
+          this story. Also we call his type of people &quot;Polibaaz&quot;, he
+          belongs to party called &quot;BJP&quot; which stands for &quot;Bihar,
+          Jharkhand, Purvanchal&quot; (ik names are hilarious). <br /> From first year
+          onward these type of people come to your room, snatch your phone and
+          vote for their fav senior. Then these type of people work their way up
+          the social ladder and then they get to the position of power. Rule is
+          simple, you work hard for someone, that someone will give you political
+          Patronage.
+          <br />
+          Some of his other qualities is that he likes to drink, smoke etc stuff
+          which I think are okay, but he does it to gain favour and make bonds,
+          I just find it cringe tbh.
+          <br />
+          He is also my batch mate(Yes we are from same branch), and he was the
+          first person I talked to in college. He was my first friend
+          technically in college. But as time passed, I saw his true colours. He
+          was weak, enable to take criticism, and he was a bully. A type of person
+          you want to keep away for your piece of mind.
+          <br />
+          <br />
+          His dad is kinda rich. He likes to brag, that also just makes me
+          want to stab myself whenever he opens his mouth.
+          <h2 className="text-xl font-semibold mb-4">Solution Part 1</h2>
+          {/* <br /> <br /> */}I found people who thought the way I thought, who
+          related with the undemocratic problem. Who were tired of eating shitty
+          food in the mess, while other messes made perfect food. One of them
+          was Bhuvan, who also was from the most populous branch in our hostel
+          and people liked him in general.
+          <br />
+          It was actually his that I should stand for this position. I was not
+          sure, but he was, he believed in me. He guided me through the entire
+          process, telling me about all the procedures, helped in manifesto,
+          while he still was in his sister&apos;s wedding.
+          <br />
+          So the list of Candidates actually leaked a day prior (I just claimed,
+          the process was nearly fair, no cheating on votes part). I just woke
+          up and got a call from Akshat, my friend from 1st year(He is a good
+          guy). He told me why have I kept this candidature secret from him.
+          Then He said Aarsh (The opponent) is in his room, he wants to talk, I
+          said I don&apso;t want to. He insisted and then I thought what the
+          heck, let&apso;s face it.
+          <br />
+          <br />
+          Aarsh pleaded that I take down my candidature (I was wondering while
+          he was speaking why withdrawal option exist). He requested, then tried
+          intimidating me and many other methods like making me realize how this
+          will be a huge humiliation and I will lose.
+          <br />
+          <br />
+          In the evening around 12 in the morning, I got another call from Aman
+          (Again a good senior). He again tried convincing me to withdraw my
+          candidature. His take was I will get exhausted by the intensity of the
+          &apos;politics&apos;. He was doing this because his friends was
+          forcing him to, (The seniors, who supported Aarsh, by support he
+          gained by doing things in Problem 1).
+          <br />
+          I went back to my room, then Aman called again, and this time those
+          &apos;seniors&apos; were in the room. They tried all possible way to
+          convince me, humiliation, exhaustion, or counting the ways Aarsh was
+          better etc, etc so this went on for 1 hour to be precise. Sounds shitty
+          right, ain&apos;t it? Feels wrong, but they were not scared. (I said EC
+          was okayish, not the best). After hearing from more people like them,
+          i didn&apos;t withdraw my candidature. Its in turn motivated me to do
+          better.
+          <br />
+          <br />
+          <h2 className="text-xl font-semibold mb-4">Solution Part 2</h2>
+          Now time for 3 day Campaigning started, I was not sure at first how to
+          approach this, I did what in general my opponent was doing, making a
+          poster and promotional video and distributing it on WhatsApp groups
+          and people Instagram.
+          <br />
+          1st day went by like nothing, everything was shitty. Heck it! I was
+          sad and feeling shitty, I was not able to do anything. People prophecy
+          was coming true. But the gate of my room was open, then a dude who
+          lives near my room came in talked to me. He was surprised that stood
+          for election and told me that the fact that a geek person was taking
+          part in elections was really enticing and that really attracted him to
+          my cause. He also told me not that not many of his friends knew that I
+          was standing for election, and they didn&apos;t even knew there were
+          election going to happen in the Bhawan.
+          <br />
+          <br />
+          From this convo I needed to tell people that I exist and I am like
+          them, I am not a politician, I am a student who is tired current
+          system. By politician, I obviously mean the people like Aarsh (who
+          works hard all their college life to get to this position). I started
+          banging on doors, I started talking to people, I told them about
+          Problem 1. Obviously this was not easy, many people hated the fact
+          that I was at their door and was disturbing their daily routine, and I
+          was surprised by the number of people who were willing to talk to me.
+          The sheer number of people who invited me inside the room, sat with me
+          and listened to me. They were all tired, some thousands of kilometres
+          away from their home of the unhygienic condition of washrooms, the bad
+          food, the lack of clean laundry. All this time, Usman and sometime
+          Pranav was with me, they helped me in talking to people, they were my
+          support system.
+          <br />
+          <br />
+          I found some observations about people via this: <br />
+          <h1 className="ml-4">
+            1) Many people won&apos;t bite you, when you go talk to them. <br />
+            2) People become more comfortable when they are in group.(Might make
+            you nervous tho) <br />
+            3) People hate when you don&apos; listen to them, they want to be
+            heard. <br />
+            4) Some people are just shitty. <br /> <br />
+          </h1>
+          I talked to nearly 300 students, listened to their problems. It was a
+          nice experience, met really nice people. <br />
+          Debate was another highlight, I was told that people will crush me in
+          the Debate by the People in Aman&apos;s room. They will shout at me,
+          interrupt me and will not let me speak. For Debate, I was scared
+          shitless, I was prepared but scared shitless. First, my opponent gave
+          his speech, in plain English which he memorized, and then I gave mine,
+          in Hindi. That was a big plus, people felt my words, and I was super
+          surprised when the people cheered instead of shouting at me. The only
+          problem during debate was Aarsh, he was forcing his friend(WatchOut
+          member, watchout conduct the debate) to not let me speak in Hindi.
+          Speaking in any language was allowed, EC is not perfect, its just good
+          enough.
+          <br />
+          <br />
+          I knew I was going to be intimidated, but the limits were crossed when
+          the Aarsh went to rooms of people who were putting up my manifesto in
+          their status. They were just showing their support and what they got
+          to show their support was a threat talk from their seniors, how sad
+          the senior was that they were supporting me. For someone not from IITR
+          this might seem like a small things but people here are connected
+          with their seniors, they are like the only guiding figure in college life.
+          Some people get good seniors(I think I got the best) some bad(Aarsh
+          got the worst ones).
+          <br />
+          <br />
+          <h2 className="text-xl font-semibold mb-4">The End</h2>
+          The thing you need to know about election is that it happens 9am in
+          the morning which I found (During Campaigning) the favourite time for
+          IITR student to sleep.
+          <br />
+          So the general strategy of these politicians is to get their
+          friends/upcoming politicians to wake up people, snatch their phone and
+          vote for Aarsh. So according to sources Aarsh had group of 40 people
+          waking people up for him. There 7 floor in RKB, and 40/7 = 6 people
+          per floor. So 6 people per floor were waking up people for Aarsh.
+          <br />
+          And as a geek, who just started into politics, I had no-one to run for
+          me. Actually, I had 3 people. Usman(A fucking ninja), Akshat went to
+          ground floor to wake up some people, and Tanishq, he called his 4
+          friends. We later joked how big and important Tanishq contribution
+          was.
+          <br />
+          So there was also some other shit, how election started 15 min before
+          it was supposed to etc. Saw lots of people roaming outside the
+          room, a lot of other stuff happened (minor). Voting time ended and I
+          slept thinking I did the best I could. Actually that was my mantra, I
+          just want to do as best I could.
+          <br />
+          <br />
+          Now comes the result day, a lot of people were refreshing the result
+          website just to see the result. I didn&apos;t knew if I won, I did
+          made the entire process a lot interesting.
+          <br />
+          Then I get a call from DoSW office, they called me in, I was scared. I
+          reached the office, I still remember I was wearing a shirt which read{" "}
+          <br />
+          &quot;I sorry but I don&apos;t care about your opinion.&quot; <br />{" "}
+          The DoSW, AdoSW, and AdoSW Hostel Affair were sitting in the office.
+          They told me there was complained against me for distributing physical
+          poster, it was true, but I didn&apos;t knew it was illigal. Nothing in
+          the guideline said that I can&apos;t distribute physical poster.{" "}
+          <br /> Well that led to a tie, and heck I was happy, I couldn&apos;t
+          believe what I was witnessing. I was feeling a mix of sadness and
+          feeling of accomplishment. I finally did something which I thought was
+          impossible.
+          <br />
+          <br />
+          <h2 className="text-xl font-semibold mb-4">The Conclusion</h2>
+          After few days of hostel warden took the interview, and he decided
+          that Aarsh was a better choice for the position. I respect his
+          decision, but I was happy that working 7 days straight, talking to
+          people, and making them aware of the problems was not a waste of time.
+          <br />
+          <br />
+          Now the result didn&apos;t turned out as I hoped for, but I saw
+          something which I never saw before. I saw people coming together and
+          asking why was the food shitty, why the water cooler never works, why
+          the washroom was always dirty. They were questioning the system, and
+          that was the biggest win for me.
+
+
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
